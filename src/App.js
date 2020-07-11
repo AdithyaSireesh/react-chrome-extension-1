03f6a54ec15a5c493f35b8628273c3b98ca4b14f
AdithyaSireesh <s1616497@sms.ed.ac.uk>
2020-07-11 15:34:23 +0100

    Add any color to body

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
 
   changeColor(e) {
-    window.chrome.tabs.executeScript(null, { code: "document.body.style.backgroundColor='" + e.target.id + "'" });
+    window.chrome.tabs.executeScript(null, { code: "document.body.style.backgroundColor='" + e.target.value + "'" });
   }
 
   render() {
@@ -16,10 +16,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React Chrome Extension</h1>
         </header>
         <div className="App-intro">
-          <div className="item" id="red" onClick={this.changeColor.bind(this)}>red</div>
-          <div className="item" id="blue" onClick={this.changeColor.bind(this)}>blue</div>
-          <div className="item" id="green" onClick={this.changeColor.bind(this)}>green</div>
-          <div className="item" id="yellow" onClick={this.changeColor.bind(this)}>yellow</div>
+          <input type="text" id="colour" name="colour" onChange={this.changeColor.bind(this)}/>
         </div>
       </div>
     );
